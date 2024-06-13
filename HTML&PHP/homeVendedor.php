948bb8013386b802e2c1204ce289f07748acbf99
joicexz <joicesoaresalmeida@gmail.com>
2024-06-13 19:40:12 -0300

    Iniciando página vendedor - 13/06

---
+++ HTML&PHP/homeVendedor.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/homeVendedor.css">
+    <link rel="icon" href="../img/logo-icon.png">
+    <title>Produtos cadastrados</title>
+</head>
+
+<body>
+
+    <nav>
+        <div class="logo">
+            <img src="../img/logo-estendida.png" alt="" class="img-logo">
+        </div>
+
+        <div class="menu">
+
+            <span onclick="window.location.href='produtosVendedor.php'">Produtos</span>
+            <!-- <span onclick="window.location.href='sacola.php'">sua sacola</span> -->
+            <span onclick="window.location.href='perfilVendedor.php'">seu perfil</span>
+        </div>
+    </nav>
+
+    <div class="feed">
+        <h1>FEED</h1>
+    </div>
+
+</body>
+
+</html>
