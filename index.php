55aadb2e2435054a3982d359201fbdb1a5d36dda
joicexz <joicesoaresalmeida@gmail.com>
2024-07-02 12:30:04 -0300

    02-07

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="icon" href="img/logo-icon.png">
+    <title>CADASTRE-SE</title>
+</head>
+
+<body>
+
+    <div class="card-cadastro">
+        <h1>Cadastro</h1>
+
+        <button class="btn-cadastro" id="btn-cadastro-comprar" onclick="window.location.href='HTML&PHP/cliente.php'">Quero comprar</button>
+        <button class="btn-cadastro" id="btn-cadastro-vender" onclick="window.location.href='HTML&PHP/vendedor.php'">Quero vender</button>
+
+        <span onclick="window.location.href='HTML&PHP/login.php'">Já possui conta? Entrar</span>
+
+    </div>
+
+</body>
+
+</html>
